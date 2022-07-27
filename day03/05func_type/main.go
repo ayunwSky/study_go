1b5a2bea7d5294b99505f50b624d261e1b05a7c1
Allen_Jol <allen_jol@163.com>
2022-07-27 17:35:43 +0800

    feat: 函数类型作为参数和返回值

---
+++ day03/05func_type/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+// 函数类型作为参数和返回值
+
+func f1() {
+	fmt.Println("I am f1 func")
+}
+
+func f2() int {
+	return 10
+}
+
+// 函数作为参数
+func f3(x func() int) {
+	ret := x()
+	fmt.Println(ret)
+}
+
+func f4(x, y int) int {
+	return x + y
+}
+
+// 函数作为返回值
+func f5(x func() int) func(int, int) int {
+	ret := func(a, b int) int {
+		return a + b
+	}
+	return ret
+}
+
+func main() {
+	a := f1
+	fmt.Printf("%T\n", a)
+	fmt.Println(a)
+
+	b := f2
+	fmt.Printf("%T\n", b)
+
+	f3(f2)
+
+	fmt.Printf("%T\n", f4)
+
+	f5Res :=f5(f2)
+	fmt.Println(f5Res)
+	fmt.Printf("%T\n", f5Res)
+}
