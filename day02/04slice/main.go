4d6f1cdf04807b54ad2fae25a47a3dcdd8c47600
Allen_Jol <allen_jol@163.com>
2022-07-27 09:37:23 +0800

    feat: 切片修改

--- day02/04slice/main.go
+++ day02/04slice/main.go
@@ -26,8 +26,8 @@ func main() {
 	fmt.Println(s2 == nil)
 
 	// 长度和容量
-	fmt.Printf("len(s1): %d cap(s1):%d\n", len(s1), cap(s1))
-	fmt.Printf("len(s2): %d cap(s2):%d\n", len(s2), cap(s2))
+	fmt.Printf("len(s1):%d cap(s1):%d\n", len(s1), cap(s1))
+	fmt.Printf("len(s2):%d cap(s2):%d\n", len(s2), cap(s2))
 
 	// 由数组得到切片
 	a1 := [...]int{1, 3, 5, 7, 9, 11, 13}
