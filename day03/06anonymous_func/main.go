30b1fb15ec99c8a506d51ce19657f85fca21fa28
Allen_Jol <allen_jol@163.com>
2022-07-28 10:10:04 +0800

    feat: 匿名函数

--- day03/06anonymous_func/main.go
+++ day03/06anonymous_func/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 // 匿名函数
+/*
+	函数内部无法定义带名字的函数，因此在函数内部就只能定义匿名函数
+	如果函数只是被调用一次，则可以简写成立即执行函数,就是写完函数后加括号立即执行
+*/
 
 // var f1 = func(x, y int) {
 // 	fmt.Println(x + y)
@@ -10,6 +14,7 @@ import "fmt"
 
 func main() {
 	// f1(10,20)
+	// 匿名函数
 	// 函数内部无法声明带名字的函数，所以匿名函数一般用在函数内部
 	f1 := func(x, y int) {
 		fmt.Println(x + y)
