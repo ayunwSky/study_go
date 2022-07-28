397ef603f1a828f44ef620e0eb1681ce708aee66
allen_jol <471733951@qq.com>
2022-07-28 22:31:16 +0800

    feat: panic 和 recover

--- day03/10panic_recover/main.go
+++ day03/10panic_recover/main.go
@@ -4,8 +4,14 @@ import "fmt"
 
 // panic 和 recover
 /*
+	panic 对异常进行捕获，类似Python中的 try ... except
 	recover() 必须搭配 defer 使用
 	defer一定要在可能引发 panic 的语句之前定义
+
+	利用recover可以捕获panic异常，保证程序可以正常执行，不会崩溃导致直接退出程序
+
+	https://www.jianshu.com/p/6c3ce7406009
+	https://yuerblog.cc/2019/09/26/golang-%E5%88%A9%E7%94%A8recover%E6%8D%95%E8%8E%B7panic%E5%BC%82%E5%B8%B8/
 */
 
 func funcA() {
