f7848df0c19a2dd5424676c9a200b22609fbb05e
ayunwSky <471733951@qq.com>
2022-08-14 14:09:05 +0800

    fix: 接口体和json序列化、反序列化

---
+++ day04/12struct_with_json/go.mod
@@ -0,0 +1,3 @@
+module github.com/ayunwSky/study_go/day04/12struct_with_json
+
+go 1.19
--- day04/12struct_with_json/main.go
+++ day04/12struct_with_json/main.go
@@ -6,10 +6,10 @@ import (
 )
 
 // 结构体与json
-/* 
+/*
 	https://sanyuesha.com/2018/05/07/go-json/
 	https://studygolang.com/articles/35184
-	
+
 	1. 把 Go 语言中的结构体变量转成 json 格式的字符串，称为序列化
 	2. 把 json 格式的字符串转换成 Go语言中能识别的结构体，称为反序列化
 
@@ -27,24 +27,29 @@ import (
 	Struct tag 可以决定 Marshal 和 Unmarshal 函数如何序列化和反序列化数据。
 */
 
+/*
+注意：这里是 person 结构体中定义的，所以在 json 这个包做序列化的时候是会出现空 json 的情况
+解决这个空 json 数据的方法就是将 name 和 age 都改成 大写字母开头的，这样 json 包就能识别到
 
+前端可能会告诉你，我前端处理的时候，都是用小写的，你给我都是大写字母开头不行，怎么解决？
+使用 struct tags，即用 json 去解析的时候，用反引号中的小写的 name 来代替 Name,
+可以写多个，比如 json中用name，db 数据库中也用 name，ini格式配置文件中也用 name
+注意：在 "json:" 后面不能加空格
+*/
+
+//type person struct {
+//	Name string `json:"name" db:"name" ini:"name"`
+//	Age  int    `json:"age"`
+//}
 
 type person struct {
-	// 注意：这里是 person 结构体中定义的，所以在 json 这个包做序列化的时候是会出现空 json 的情况
-	// 解决这个空 json 数据的方法就是将 name 和 age 都改成 大写字母开头的，这样 json 包就能识别到
-
-	// 前端可能会告诉你，我前端处理的时候，都是用小写的，你给我都是大写字母开头不行，怎么解决？
-	// 使用 struct tags，即用 json 去解析的时候，用反引号中的小写的 name 来代替 Name,
-	// 可以写多个，比如 json中用name，db 数据库中也用 name，ini格式配置文件中也用 name
-	// 注意：在 "json:" 后面不能加空格
-	
-	Name string `json:"name" db:"name" ini:"name"`
+	Name string `json:"name"`
 	Age  int    `json:"age"`
 }
 
-type Message struct{
+type Message struct {
 	Name string `json:"name"`
-	Body string	`json:"body"`
+	Body string `json:"body"`
 }
 
 func main() {
@@ -71,6 +76,8 @@ func main() {
 	json.Unmarshal([]byte(str), &p2)
 	fmt.Printf("%#v\n", p2)
 	fmt.Printf("%v\n", p2)
+	fmt.Println(p2.Name)
+	fmt.Println(p2.Age)
 
 	fmt.Println()
 
@@ -81,7 +88,7 @@ func main() {
 
 	b2, err := json.Marshal(m)
 
-	if err != nil{
+	if err != nil {
 		fmt.Printf("err: %v\n", err)
 		return
 	}
