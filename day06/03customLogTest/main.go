5362418d34af6735d1c893e0440e53d8f78fd715
ayunwSky <471733951@qq.com>
2022-08-14 13:29:40 +0800

    fix: 自定义日志库实现方法

--- day06/03customLogTest/main.go
+++ day06/03customLogTest/main.go
@@ -10,12 +10,16 @@ import (
 	"github.com/ayunwSky/study_go/day06/mylogger"
 )
 
+// 声明一个全局的接口变量
+var log mylogger.Logger
+
 // 测试自己定义的日志库
 func main() {
 	// 1. 往终端打印日志
 	//id := 1101
 	//name := "ayunwSky"
-	//log := custom_log.NewLog("INFO")
+	////log := mylogger.NewConsoleLog("INFO")
+	//log = mylogger.NewConsoleLog("INFO")	// 终端日志实例
 	//
 	//for {
 	//	log.Debug("这是一条 Debug 级别的测试日志")
@@ -28,7 +32,10 @@ func main() {
 	//}
 
 	// 2. 日志写入到文件。实现一个日志文件500MB
-	log := mylogger.NewFileLogger("INFO", "./", "access.log", 500*1024*1024)
+	//log := mylogger.NewFileLogger("INFO", "./", "access.log", 500*1024*1024)
+
+	// 文件日志实例
+	log = mylogger.NewFileLogger("INFO", "./", "access.log", 500*1024*1024)
 
 	for {
 		log.Debug("这是一条 Debug 级别的测试日志")
